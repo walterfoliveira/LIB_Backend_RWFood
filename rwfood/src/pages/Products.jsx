a4c53b4f39bdca0e7cd55cec32f77db31cf20521
Walter D F de Oliveira <walter@rwconsultoria.com.br>
2023-08-11 15:00:11 -0300

    Ajuste na telade Product, Titulo do Sistema

--- rwfood/src/pages/Products.jsx
+++ rwfood/src/pages/Products.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
+import RecentOrders from '../components/RecentOrders'
 
 export default function Products() {
-	return <div>Products page</div>
+    return (
+        <div>
+            <div className="flex flex-row gap-4 w-full">
+                <RecentOrders />
+            </div>
+        </div>
+    )
 }
